938a793717abb2dbbcc4665e47fafcedb05859c7
Abdul Rahman <programmerabubassam@gmail.com>
2025-12-19 09:26:26 +0300

    Add WyChooseUs component to HomePage

--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -1,12 +1,14 @@
 import Navbar from "../components/Navbar/Navbar.jsx";
 import Hero from "../components/hero/Hero.jsx";
 import MostRentedCars from "../components/mostRentedCars/MostRentedCars.jsx";
+import WyChooseUs from "../components/whyChooseUs/WhyChooseUs.jsx";
 
 const HomePage = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <WyChooseUs />
       <MostRentedCars />
     </>
   );
