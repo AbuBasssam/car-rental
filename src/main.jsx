816900dc7d92dc26483d72dff4233408c0481509
Abdul Rahman <programmerabubassam@gmail.com>
2026-01-22 14:10:07 +0300

    refactor main file

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,8 @@
-import { StrictMode } from "react";
-import "./index.css";
-import App from "./App.jsx";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/homePage.jsx";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      // {
-      //   path: "students",
-      //   element: <StudentsPage />,
-      // },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import "./index.css";
+import { router } from "./routes/router";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
