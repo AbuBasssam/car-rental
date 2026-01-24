8f089d9ccabd55814ae3044aa78b226910e4850f
Abdul Rahman <programmerabubassam@gmail.com>
2026-01-24 08:46:11 +0300

    Add initializeSecurity Method which request CSRF Token

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import { Outlet } from "react-router-dom";
+import { initCsrfToken } from "./services/authService";
 
 function App() {
   useEffect(() => {
@@ -9,6 +10,9 @@ function App() {
       duration: 1000,
       once: true,
     });
+    const initializeSecurity = async () => await initCsrfToken();
+
+    initializeSecurity();
   }, []);
 
   return (
